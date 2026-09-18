21322f70aea5a5e5e01827c0622393416ea4a73a
Luis Fernando Sotelo Rojas <luisfernandoso11@outlook.com>
2026-09-17 19:24:28 -0500

    Configurar inicio de aplicación y rutas principales

---
+++ backend/gestionhotelesbackend/src/main/java/com/gestionhoteles/backend/GestionHotelesApplication.java
@@ -0,0 +1,12 @@
+package com.gestionhoteles.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionHotelesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionHotelesApplication.class, args);
+    }
+}
